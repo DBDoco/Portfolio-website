29e2ce473d347b2e4287ac6ac9fdb690d8d8edc0
Dominik Bedenic <60035381+DBDoco@users.noreply.github.com>
2021-11-08 10:59:12 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+
+<h1 align="center">
+  Personal portfolio website
+  <br>
+</h1>
+
+<h4 align="center">Single front-end only web page made with HTML, CSS, JavaScript and using elements from Bootstrap 5. Responsive for phones, tablets and computer screens. Animations are made with <a href="https://greensock.com/gsap/">GSAP</a>.</h4>
+
+<p align="center">
+  <img src="https://media0.giphy.com/media/9rqZ3NPMZIU7vzg72I/giphy.gif?cid=790b7611c9b3897fabfd16e15d31012c4c07653c435ecb6b&rid=giphy.gif&ct=g" alt="fight" />
+</p>
+
+
+
+
+## How To Use
+
+Clone this repository
+```bash
+$ git clone https://github.com/DBDoco/Portfolio-website.git
+```
+
+Go into the repository
+```bash
+$ cd portfolio-development
+```
+
+To view the page just open index.html in your favorite browser.
---
+++ index.js
@@ -0,0 +1,84 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".nav-link", {  scrollTrigger:{
+    trigger:".circle-1",
+    toggleActions:"restart none none restart"
+} ,duration: 1, opacity: 0 });
+gsap.from(".navbar-button", {  scrollTrigger:{
+    trigger:".circle-1",
+    toggleActions:"restart none none restart"
+} ,duration: 1, opacity: 0 });
+
+gsap.from(".circle-1", { scrollTrigger:{
+    trigger:".circle-1",
+    toggleActions:"restart none none restart"
+} ,duration: 1.1, x: "-20%", ease: "sine" });
+gsap.from(".circle-1", {  scrollTrigger:{
+    trigger:".circle-1",
+    toggleActions:"restart none none restart"
+} ,duration: 0.5, opacity: 0 });
+
+gsap.from("h1", {  scrollTrigger:{
+    trigger:".circle-1",
+    toggleActions:"restart none none restart"
+} ,duration: 1.3, x: "-10%" });
+
+gsap.from("h1", {  scrollTrigger:{
+    trigger:".circle-1",
+    toggleActions:"restart none none restart"
+} ,duration: 1, opacity: 0 });
+
+gsap.from(".btn-heading-1", {scrollTrigger:{
+    trigger:".btn-heading-1",
+    toggleActions:"restart none none restart"
+},duration:1, opacity:0})
+
+gsap.from(".btn-heading-2", {scrollTrigger:{
+    trigger:".btn-heading-2",
+    toggleActions:"restart none none restart"
+},duration:1, opacity:0})
+
+gsap.from("h2", { scrollTrigger:{
+    trigger:"h2",
+    toggleActions:"restart none none restart"
+} ,duration: 1.1, x: "-20%", ease: "sine" });
+gsap.from("h2", {  scrollTrigger:{
+    trigger:"h2",
+    toggleActions:"restart none none restart"
+} ,duration: 0.5, opacity: 0 });
+
+gsap.from(".about-me", { scrollTrigger:{
+    trigger:".about-me",
+    toggleActions:"restart none none restart"
+} ,duration: 1.1, x: "20%", ease: "sine" });
+gsap.from(".about-me", {  scrollTrigger:{
+    trigger:".about-me",
+    toggleActions:"restart restart none restart"
+} ,duration: 1, opacity: 0});
+
+gsap.from(".circle-2", { scrollTrigger:{
+    trigger:".circle-2",
+    toggleActions:"restart none none restart"
+} ,duration: 1.1, x: "20%", ease: "sine" });
+gsap.from(".circle-2", {  scrollTrigger:{
+    trigger:".circle-2",
+    toggleActions:"restart none none restart"
+} ,duration: 0.5, opacity: 0 });
+
+gsap.from(".contact", { scrollTrigger:{
+    trigger:".contact",
+    toggleActions:"restart none none restart"
+} ,duration: 1.1, y: "-20%", ease: "sine" });
+gsap.from(".contact", {  scrollTrigger:{
+    trigger:".contact",
+    toggleActions:"restart none none restart"
+} ,duration: 1, opacity: 0});
+
+gsap.from(".card", { scrollTrigger:{
+    trigger:".card",
+    toggleActions:"restart none none restart"
+} ,duration: 1.1, y: "-50%", ease: "sine" });
+gsap.from(".card", {  scrollTrigger:{
+    trigger:".card",
+    toggleActions:"restart none none restart"
+} ,duration: 1, opacity: 0});
