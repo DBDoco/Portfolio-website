6fa0508d6f42716753f749b2027e4a4d851506fb
Dominik Bedenic <60035381+DBDoco@users.noreply.github.com>
2023-03-10 11:53:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 /* Animations */
-window.addEventListener('load', () => {
-    AOS.init({
-        startEvent: 'load',
-        duration: 800,
-      });
+AOS.init({
+    startEvent: 'load',
+    duration: 800,
 });
 
 
 
 /* Project section filtering */
-$(document).on('click', '.projects-filter li', function(){
+$(document).on('click', '.projects-filter li', function () {
     $(this).addClass('projects-filter-active').siblings().removeClass('projects-filter-active')
 })
 
-$(document).ready(function(){
-    $('.list').click(function(){
+$(document).ready(function () {
+    $('.list').click(function () {
         const value = $(this).attr('data-filter');
-        if(value=='all'){
+        if (value == 'all') {
             $('.projects-card').show('1000');
-        } else{
-            $('.projects-card').not('.'+value).hide('1000');
-            $('.projects-card').filter('.'+value).show('1000');
+        } else {
+            $('.projects-card').not('.' + value).hide('1000');
+            $('.projects-card').filter('.' + value).show('1000');
         }
     })
 })
